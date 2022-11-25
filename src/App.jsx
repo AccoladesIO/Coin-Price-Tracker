0ae0f54bdabd8a6fca851635afda9966b27f045d
unknown <ononobip@gmail.com>
2022-11-26 00:11:32 +0100

    ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Stats from './components/Stats'
 
 function App() {
 
   return (
     <>
-      <Navbar></Navbar>
-      <Hero />
+      <Router>
+        <Routes>
+          <Route path='/' exact
+            element={
+              <>
+                <Navbar />
+                <Hero />
+              </>
+            } />
+          <Route path='/stats'
+            element={
+              <>
+                <Navbar />
+                <Stats />
+              </>
+            } />
+          <Route path='*' element={
+            <Error />
+          } />
+        </Routes>
+      </Router>
+
     </>
   )
 }

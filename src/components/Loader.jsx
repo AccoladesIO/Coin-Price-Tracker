fedf77bb4192ee37f45b5bf43583df47e284e3d8
unknown <ononobip@gmail.com>
2022-12-29 01:28:34 +0100

    loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div className='loader-section'>
+            <div className="planet"></div>
+        </div>
+    )
+}
+
+export default Loader

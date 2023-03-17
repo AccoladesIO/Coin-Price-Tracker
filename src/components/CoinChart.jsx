f3fdc8027561d8176b65a6b7f4eee1c3ad350909
unknown <ononobip@gmail.com>
2023-03-17 23:16:21 +0100

    Fixes: updated a component

--- src/components/CoinChart.jsx
+++ src/components/CoinChart.jsx
@@ -1,44 +1,100 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
 import { Line } from 'react-chartjs-2';
-import axios from 'axios';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 
 const CoinChart = () => {
-  const [chartData, setChartData] = useState({});
+  const [chart, setChart] = useState({})
+  var baseUrl = "https://api.coinranking.com/v2/coins/?limit=10";
+//   var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+//   var apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
-      );
-  
-      if (response.data.length > 0 && response.data[0].sparkline_in_7d) {
-        const labels = response.data[0].sparkline_in_7d.price.map(
-          (item, index) => `Day ${index}`
-        );
-        const datasets = response.data.map((coin) => {
-          return {
-            label: coin.name,
-            data: coin.sparkline_in_7d.price,
-            fill: false,
-            borderColor: coin.color,
-          };
+    const fetchCoins = async () => {
+      await fetch(baseUrl)
+        .then((response) => {
+          if (response.ok) {
+            response.json().then((json) => {
+              console.log(json.data);
+              setChart(json.data)
+            });
+          }
+        }).catch((error) => {
+          console.log(error);
         });
-  
-        setChartData({
-          labels,
-          datasets,
-        });
-      }
     };
-  
-    fetchData();
-  }, []);
-  
-  return (
-    <div>
-      <Line data={chartData} />
-    </div>
-  );
-};
+    fetchCoins()
+  }, [baseUrl])
 
-export default CoinChart;
+  console.log("chart", chart);
+  var data = {
+    labels: chart?.coins?.map(x => x.name),
+    datasets: [{
+      label: `${chart?.coins?.length} Coins Available`,
+      data: chart?.coins?.map(x => x.price),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  var options = {
+    maintainAspectRatio: false,
+    scales: {
+    },
+    legend: {
+      labels: {
+        fontSize: 25,
+      },
+    },
+  }
+
+  return (
+    <div className='chart__wrapper'>
+      <Line
+        data={data}
+        height={400}
+        options={options}
+
+      />
+    </div>
+  )
+}
+
+export default CoinChart
